5d62cf245f455f1313acd125f3ec490bb0fc6aec
Julian <93753805+Julesarg@users.noreply.github.com>
2022-09-19 15:06:42 -0300

    change constructor name

--- desafio3.js
+++ desafio3.js
@@ -1,6 +1,6 @@
 const express = require(`express`);
 const app = express();
-const Contenedor = require("./ejemplo");
+const Contenedor = require("./constructor");
 
 const contenedor = new Contenedor(`productos.txt`);
 const productos = contenedor.getAll();
